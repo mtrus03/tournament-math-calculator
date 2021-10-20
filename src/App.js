f68c8d6c3a3b6a646e551b75508c4c72036494fa
Travis Russell <mtrussell@gmail.com>
2021-10-20 16:20:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Label, Input, FormGroup } from 'reactstrap';
+import { React, useState } from 'react';
+// import $ from 'jquery';
 
 function App() {
+  const [inputState, setInputState] = useState({ players: 32, rounds: 5, topCut: 8 });
+  const { players, rounds, topCut } = inputState;
+
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+    setInputState({ ...inputState, [name]: value });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-2">
+      <Row className="mx-auto text-center">
+        <Col className="col-4 bg-primary text-light">
+          <FormGroup className="mb-4">
+            <Label for="players" className="h2">Players</Label>
+            <Input name="players" type="text" id="players" value={inputState.players} min="2" className="w-25 mx-auto" onChange={handleFormChange} />
+          </FormGroup>
+          <FormGroup className="mb-4">
+            <Label for="rounds" className="h2">Rounds</Label>
+            <Input name="rounds" type="number" id="rounds" value={inputState.rounds} min="2" className="w-25 mx-auto" onChange={handleFormChange} />
+          </FormGroup>
+          <FormGroup className="mb-2">
+            <Label for="topCut" className="h2">Top Cut</Label>
+            <Input name="topCut" type="number" id="topCut" value={inputState.topCut} min="2" className="w-25 mx-auto" onChange={handleFormChange} />
+          </FormGroup>
+        </Col>
+        <Col className="col-8">
+          <p className="h2">Fart</p>
+          <span>Players: {players}<br /></span>
+          <span>Rounds: {rounds}<br /></span>
+          <span>Top Cut: {topCut}<br /></span>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
